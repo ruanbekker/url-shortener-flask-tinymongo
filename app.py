29aec28f8305553bfd00217bfda3bf7ad072399b
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-06-05 01:02:17 +0200

    add create endpoint

--- app.py
+++ app.py
@@ -7,11 +7,26 @@ c = TinyMongoClient('tinydb')
 db_init = c.mydb
 db = db_init.records
 
+def create_tiny(full_url):
+    url_id = generate_id()
+    check_id_existence = check_duplicates(url_id)
+    if check_id_existence == 'no':
+        tiny_url = domain + url_id
+        doc = db.insert_one({'url_id': url_id, 'url_original': full_url, 'url_short': tiny_url}).inserted_id
+        return 'Your TinyURL is: <a href="{}">{}</a>'.format(tiny_url, tiny_url)
+    else:
+        return 'URL Already Exists: {0}'.format(check_id_existence['url_short'])
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'OK'
+
+@app.route('/create/<path:fullurl>')
+def index(fullurl):
+    response = create_tiny(fullurl)
+    return response
 
 if __name__ == '__main__':
     app.run()
